6fa2b50434b72626dae06f44be35f9996a11fb5f
Santosh Kulkarni <santoshvqa@gmail.com>
2025-08-25 09:13:36 +0530

    added safari

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,6 +77,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
     {
+      name: 'Safari',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
